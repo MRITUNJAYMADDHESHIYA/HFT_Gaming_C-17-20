2fe89e7577acecd9484fc2cd1b4d31abb53e7d44
Mritunjay Maddhesiya <mritunjay112002@gmail.com>
2026-03-07 09:26:29 +0530

    this investment task is completed

--- Portfolio_Constraint/src/mcap_filter.py
+++ Portfolio_Constraint/src/mcap_filter.py
@@ -5,29 +5,25 @@ def apply_mcap_filter(portfolio, year, mcap_df):
 
     if year not in mcap_df.columns:
         return []
-
     year_mcap = mcap_df[['CO_NAME', year]].dropna()
-
     year_mcap = year_mcap.sort_values(by=year, ascending=False)
 
+    ############ Rank position ##################
     year_mcap['rank'] = range(1, len(year_mcap) + 1)
 
+    ############ bottom 20% ###################
     cutoff = int(len(year_mcap) * 0.8)
 
     allowed = set(year_mcap.iloc[:cutoff]['CO_NAME'])
 
     filtered = []
-
     for stock in portfolio:
-
         if stock not in allowed:
             continue
 
         mcap_value = year_mcap.loc[year_mcap['CO_NAME'] == stock, year]
-
         if mcap_value.empty:
             continue
-
         filtered.append(stock)
 
     return filtered
--- Portfolio_Constraint/src/portfolio_processor.py
+++ Portfolio_Constraint/src/portfolio_processor.py
@@ -7,16 +7,13 @@ from src.sector_constraint import apply_sector_constraint
 def process_portfolios(invest_df, sector_df, mcap_df):
 
     years = invest_df.columns[1:]
-
     output_rows = []
 
     for _, row in invest_df.iterrows():
-
         strat_name = row['strat_name']
         new_row = {'strat_name': strat_name + "_constrained"}
 
         for year in years:
-
             stocks = row[year]
 
             if pd.isna(stocks):
@@ -32,10 +29,10 @@ def process_portfolios(invest_df, sector_df, mcap_df):
                 new_row[year] = "[]"
                 continue
 
-            # Step 1 MCAP Filter
+            ######################### Applly MCAP filter #######################################
             filtered = apply_mcap_filter(stock_list, year, mcap_df)
 
-            # Step 2 Sector Constraint
+            ######################### secotor constraint #######################################
             filtered = apply_sector_constraint(filtered, year, sector_df, mcap_df)
 
             new_row[year] = str(filtered)
