f24b09c8d3fa7d83049f526f4fbb3b5d6bdf9031
Mritunjay Maddhesiya <mritunjay112002@gmail.com>
2026-04-11 18:37:41 +0530

    some new idea

---
+++ Trade_Mannual.cpp
@@ -0,0 +1,32 @@
+//1.TheTradeRoom                
+
+
+
+
+
+//2.PipStone
+
+
+
+
+
+//3.FundingPips weekly
+
+
+
+
+
+
+//4.FundingPips Biweekly
+
+
+
+
+
+///////////////////////////////////// Rules //////////////////////////
+//lots === 0.05 
+//Risk === 20$ -- 30$
+//target === 40$ -- 50$
+
+//max-loss per day == 100$
+//target per dat   == 100$
