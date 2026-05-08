6e4a2095ae167f090727c1be2603aa2259f98979
Mritunjay Maddhesiya <mritunjay112002@gmail.com>
2026-05-08 21:20:57 +0530

    Now I am doing for system programming for my own servers and HFT funds

--- Financial_Programming/3_Finance/read.cpp
+++ Financial_Programming/3_Finance/read.cpp
@@ -1,4 +1,4 @@
-//1.Templates:
-//2.shared pointers:-
-//3.operator overloading
+//1.Templates:- it's like a datatype(compile time)
+//2.shared pointers:-allow multiple pointers to share ownership of the same object.
+//3.operator overloading:- how the operator work for user define type
 //4.c++20
